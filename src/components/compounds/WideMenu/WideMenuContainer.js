8003fd49986ba82555c8acc0f138a875eda48dce
LJEsp <esparragoleandroj@gmail.com>
2019-05-08 21:22:33 +0800

    Add WideMenuContainer animation

--- src/components/compounds/WideMenu/WideMenuContainer.js
+++ src/components/compounds/WideMenu/WideMenuContainer.js
@@ -1,19 +1,29 @@
 import React from "react";
 import styled from "styled-components";
+import { useTransition, animated } from "react-spring";
 
 const S = {};
 
-S.WideMenuContainer = styled.div``;
+S.WideMenuContainer = animated(styled.div`
+  pointer-events: ${p => (p.isHovered ? "auto" : "none")};
+`);
 
 const WideMenuContainer = props => {
   const { isHovered, bind, children } = props;
 
-  return (
-    isHovered && (
-      <S.WideMenuContainer {...bind} {...props}>
-        {children}
-      </S.WideMenuContainer>
-    )
+  const transitions = useTransition(isHovered, null, {
+    from: { opacity: 0, transform: "translateY(-1rem)" },
+    enter: { opacity: 1, transform: "translateY(0)" },
+    leave: { opacity: 0, transform: "translateY(-1rem)" }
+  });
+
+  return transitions.map(
+    ({ item, key, props: style }) =>
+      item && (
+        <S.WideMenuContainer {...bind} key={key} style={style} {...props}>
+          {children}
+        </S.WideMenuContainer>
+      )
   );
 };
 
